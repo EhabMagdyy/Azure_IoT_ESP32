b7d4b5871c2959e75cbf9f6a58e8de5041fc3003
EhabMagdyy <ehabmagdy0044@gmail.com>
2024-10-31 05:10:44 +0300

    first commit

---
+++ iot_configs.h
@@ -0,0 +1,20 @@
+/*****************************************************************************************************************************
+**********************************    Author  : Ehab Magdy Abdullah                      *************************************
+**********************************    Linkedin: https://www.linkedin.com/in/ehabmagdyy/  *************************************
+**********************************    Youtube : https://www.youtube.com/@EhabMagdyy      *************************************
+******************************************************************************************************************************/
+
+// Enter Your Wifi and Password
+#define IOT_CONFIG_WIFI_SSID "Your_Wifi_SSID"
+#define IOT_CONFIG_WIFI_PASSWORD "Your_WIfi_Password"
+
+
+// Azure IoT Hub
+#define IOT_CONFIG_IOTHUB_FQDN "<Your_IoT_Hub_Name>.azure-devices.net"
+// Azure IoT Gub Device ID
+#define IOT_CONFIG_DEVICE_ID "Your_Device_ID"
+// Azure IoT Hub Device Key (primary key)
+#define IOT_CONFIG_DEVICE_KEY "Your_Device_Key"
+
+// Publish 1 message every 5 seconds
+#define TELEMETRY_FREQUENCY_MILLISECS 5000
